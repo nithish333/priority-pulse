10045caa2e7a2d146232d1731f7bc420a708205c
nithish333 <121003145@sastra.ac.in>
2020-08-15 13:37:35 +0530

    Updates

---
+++ precache-manifest.26e3ea8f9eafb261238ca862d5096060.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c850b0b573a482da781cf82b37a4599",
+    "url": "/priority-pulse/index.html"
+  },
+  {
+    "revision": "a84266cf053f98a2fa30",
+    "url": "/priority-pulse/static/css/main.b1c1e2de.chunk.css"
+  },
+  {
+    "revision": "67574460b4500b827716",
+    "url": "/priority-pulse/static/js/2.e633fff8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/priority-pulse/static/js/2.e633fff8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a84266cf053f98a2fa30",
+    "url": "/priority-pulse/static/js/main.1ad44576.chunk.js"
+  },
+  {
+    "revision": "299dc2817becc8f2c454",
+    "url": "/priority-pulse/static/js/runtime-main.45f6664e.js"
+  },
+  {
+    "revision": "c287d469a47fdc9c01875dfffb4b7216",
+    "url": "/priority-pulse/static/media/Ambulance.c287d469.png"
+  },
+  {
+    "revision": "3d9a511a8d12d62198331f33f363fe58",
+    "url": "/priority-pulse/static/media/Profile.3d9a511a.png"
+  }
+]);
